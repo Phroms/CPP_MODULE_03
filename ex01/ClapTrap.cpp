be10a740106bc084e5c2c61674299e9eb9d83a0e
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-08-20 19:52:39 +0200

    Makefile hecho

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 18:45:48 by agrimald          #+#    #+#             */
+/*   Updated: 2024/08/20 18:45:50 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ClapTrap.hpp"
+
+std::string mensaje(std::string msg)
+{
+    msg = "funcionaaa";
+
+    std::cout << "Viendo el menasje: " << msg << std::endl;
+    return (msg);
+}
---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 18:46:00 by agrimald          #+#    #+#             */
+/*   Updated: 2024/08/20 18:46:01 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLAPTRAP_HPP
+ # define CLAPTRAP_HPP
+
+ # include <iostream>
+ # include <string>
+
+std::string mensaje(std::string msg);
+
+ # endif
